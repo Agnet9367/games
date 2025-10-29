0225a40a24c960dacfaaa16f0db207a9f092e798
Agnet9367 <arsenhavyr@gmail.com>
2025-10-29 00:52:46 +0000

    slotsss

--- gambling.py
+++ gambling.py
@@ -6,7 +6,7 @@ print("---Gambling---")
 user_input = input(f"What game do you want to play you have {coins} coins!\n\nslot machine\n")
 if user_input == "slot machine":
     while True:
-        user_input_slot = input(f"type 'start' to spin the slot for {betting_amount} coin, you have {coins} coins!\nType 'change' if you want to change betting amount\n ")
+        user_input_slot = input(f"Type 'start' to spin the slot for {betting_amount} coin, you have {coins} coins!\nType 'change' if you want to change betting amount\nType 'exit' if you want to exit\n")
         if user_input_slot == "start" and coins > 0:
             coins -= betting_amount
             slots = ["$", "%", "#"]
@@ -24,9 +24,9 @@ if user_input == "slot machine":
                 print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", slot1, slot2, slot3fake);time.sleep(0.1)
             print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", slot1, slot2, slot3)
             if slot1 == slot2 == slot3:
-                print("You won 3 coins!"); coins += betting_amount * 3
+                print("You won your bet amount times 3!"); coins += betting_amount * 3
             elif slot1 == slot2 or slot1 == slot3 or slot2 == slot3:
-                print("You won 2 coins!"); coins += betting_amount *2
+                print("You won your bet amount times 2!"); coins += betting_amount *2
             else:
                 print("You didn't win!")
         elif user_input_slot == "change":
