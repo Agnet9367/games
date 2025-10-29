c8656b454d46384cb7e2789f127ce1b8346286cc
Agnet9367 <arsenhavyr@gmail.com>
2025-10-29 00:24:39 +0000

    slots

--- gambling.py
+++ gambling.py
@@ -5,13 +5,32 @@ coins = 10
 print("---Gambling---")
 user_input = input(f"What game do you want to play you have {coins} coins!\n\nslot machine\n")
 if user_input == "slot machine":
-    user_input_slot = input(f"type 'start' to spin the slot for 1 coin, you have {coins} coins! ")
-    if user_input_slot == "start":
-        slots = ["$", "%", "#"]
+    while True:
+        user_input_slot = input(f"type 'start' to spin the slot for 1 coin, you have {coins} coins! ")
+        if user_input_slot == "start" and coins > 0:
+            coins -= 1
+            slots = ["$", "%", "#"]
 
-        slot1 = random.choice(slots);slot2 = random.choice(slots);slot3 = random.choice(slots)
-        for i in range(100):
-            slot1fake = random.choice(slots);slot2fake = random.choice(slots);slot3fake = random.choice(slots)
-            print("\n\n\n\n\n\n\n\n\n\n\n\n\n", slot1fake, slot2fake, slot3fake );time.sleep(0.05)
+
+            slot1 = random.choice(slots);slot2 = random.choice(slots);slot3 = random.choice(slots)
+            for i in range(25):
+                slot1fake = random.choice(slots);slot2fake = random.choice(slots);slot3fake = random.choice(slots)
+                print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", slot1fake, slot2fake, slot3fake );time.sleep(0.1)
+            for i in range(25):
+                slot1fake = random.choice(slots);slot2fake = random.choice(slots);slot3fake = random.choice(slots)
+                print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", slot1, slot2fake, slot3fake);time.sleep(0.1)
+            for i in range(25):
+                slot1fake = random.choice(slots);slot2fake = random.choice(slots);slot3fake = random.choice(slots)
+                print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", slot1, slot2, slot3fake);time.sleep(0.1)
+            print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", slot1, slot2, slot3)
+            if slot1 == slot2 == slot3:
+                print("You won 3 coins!"); coins += 3
+            elif slot1 == slot2 or slot1 == slot3 or slot2 == slot3:
+                print("You won 2 coins!"); coins += 2
+            else:
+                print("You didn't win!")
+        else:
+            print("You don't have enough coins!")
+
 else:
     print("Error")
